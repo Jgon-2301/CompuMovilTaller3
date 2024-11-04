d21a5c9966f9cc125c16c227b13343d784fb5414
CryoWarrior <diegoalejandrojararojas@gmail.com>
2024-11-03 21:05:30 -0500

    NO

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CompuMovilTaller3
+
+##Integrantes
+- Juan Felipe Gonzalez Quintero
+- Diego Alejandro Jara Rojas
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.taller3"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.taller3"
